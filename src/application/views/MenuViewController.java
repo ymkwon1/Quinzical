36cd6d81867d3b7416c7c38457b642830f3c0fee
Ymkwon1 <ykwo462@aucklanduni.ac.nz>
2020-09-26 20:25:09 +1200

    created return button in question scene

--- src/application/views/MenuViewController.java
+++ src/application/views/MenuViewController.java
@@ -19,12 +19,6 @@ public class MenuViewController {
 
     @FXML
     void practiceBtnClick(ActionEvent event) throws IOException {
-    	try {
-			PracticeModule model = new PracticeModule();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		Parent practiceModuleView = FXMLLoader.load(getClass().getResource("PracticeModuleView.fxml"));
 		Scene practiceModuleScene = new Scene(practiceModuleView);
 		
--- src/application/views/QuestionViewController.java
+++ src/application/views/QuestionViewController.java
@@ -1,9 +1,17 @@
 package application.views;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class QuestionViewController {
 
@@ -21,5 +29,19 @@ public class QuestionViewController {
 
     @FXML
     private Label hintsLabel;
+
+    @FXML
+    private Button returnBtn;
+
+    @FXML
+    void returnBtnClick(ActionEvent event) throws IOException {
+    	Parent menuView = FXMLLoader.load(getClass().getResource("MenuView.fxml"));
+    	Scene menuScene = new Scene(menuView);
+    	
+    	Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+    	
+    	window.setScene(menuScene);
+    	window.show();
+    }
 
 }
