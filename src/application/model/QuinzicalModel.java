da339faa3e0e37231449ec4901808a263b044b63
Ymkwon1 <ykwo462@aucklanduni.ac.nz>
2020-10-04 05:21:27 +1300

    [fix]used FileWriter to write files

--- src/application/model/QuinzicalModel.java
+++ src/application/model/QuinzicalModel.java
@@ -1,9 +1,11 @@
 package application.model;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -263,30 +265,29 @@ public class QuinzicalModel {
 	}
 	
 	public void addAnsweredQuestions (int index) {
-//		File file = new File("answered_questions");
-//		try {
-//			Scanner myReader = new Scanner(file);
-//			int count  = 0;
-//			while(myReader.hasNextLine()) {
-//				String fileLine = myReader.nextLine();
-//				_answeredQuestions[count] = Integer.parseInt(fileLine);
-//				count++;
-//			}
-//			myReader.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
 		File file = new File ("answered_questions");
-		_answeredQuestions[index]=_answeredQuestions[index]+1;
-		file.delete();
+		FileWriter fw;
 		try {
-			file.createNewFile();
-			for (int i = 0;i<5;i++) {
-				executeBashCmdNoOutput(String.format("echo \"%d\" >> answered_questions", _answeredQuestions[i]));
+			fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			_answeredQuestions[index]=_answeredQuestions[index]+1;
+			file.delete();
+			try {
+				file.createNewFile();
+				for (int i = 0;i<5;i++) {
+					bw.write(_answeredQuestions[i]);
+					if (i != 4) {
+						bw.newLine();
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
+			bw.close();
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
 	}
 	
