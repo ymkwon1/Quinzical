b0e5bf46940bfbfcd8ecd6e7a008f00d44c8edf0
Ymkwon1 <ykwo462@aucklanduni.ac.nz>
2020-10-15 01:44:35 +1300

    player class

---
+++ src/application/model/Player.java
@@ -0,0 +1,14 @@
+package application.model;
+
+public class Player {
+	private String _currentPlayer;
+	
+	
+	public String getCurrentPlayer(String player) {
+		return _currentPlayer;
+	}
+	
+	public void setCurrentPlayer(String player) {
+		_currentPlayer = player;
+	}
+}
